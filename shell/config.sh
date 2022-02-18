ee9c834cd518c1e0431ff9178cc2cf786ae8d193
GarudaIsmail <99801349+GarudaIsmail@users.noreply.github.com>
2022-02-18 15:50:02 +0530

    Update config.sh

--- shell/config.sh
+++ shell/config.sh
@@ -7,7 +7,7 @@ aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$rolename --role-ses
 )
 )
 
-python3 /var/lib/jenkins/workspace/terraform/bin/s3bucket.py
+python3 /var/lib/jenkins/workspace/bin/s3bucket.py
 
 # terraform init
 # terraform plan
