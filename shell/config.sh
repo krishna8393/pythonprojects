fe34e73285bdb96c3f326d7c81570d24fba01b1b
krishna8393 <77263470+krishna8393@users.noreply.github.com>
2022-02-21 22:16:16 +0530

    updated config.sh

--- shell/config.sh
+++ shell/config.sh
@@ -7,7 +7,7 @@ aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$rolename --role-ses
 )
 )
 
-python3 /var/lib/jenkins/workspace/s3-python/bin/s3bucket.py '${params.accountid}' '${params.region}' '${params.trg_accountid}' '${params.ami_name}'
+python3 /var/lib/jenkins/workspace/s3-python/bin/s3bucket.py '${params.accountid}' '${params.region}' '${params.SERVICE_CODE}' '${params.QUOTAS_CODE}' '${params.DESIRED_LIMIT}'
 
 # terraform init
 # terraform plan
